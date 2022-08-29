6813f57aab60fe046e4336103ed5630a85623325
Artur Yesipov <arturyes1993@gmail.com>
2022-08-29 15:46:36 +0300

    Added new images, added feature of global click, basic changing with arrow

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import {useState} from "react";
-import NavigationBar from "./organisms/NavigationBar";
-import ExpandGalleryBtn from "./organisms/ExpandGalleryBtn";
-import GalleryTab from "./organisms/GalleryTab";
-import img1 from "./images/1.jpg";
-import img2 from "./images/2.jpg";
-import img3 from "./images/3.jpg";
-import img4 from "./images/4.jpg";
-
+import { useState } from 'react';
+import NavigationBar from './organisms/NavigationBar';
+import ExpandGalleryBtn from './organisms/ExpandGalleryBtn';
+import GalleryTab from './organisms/GalleryTab';
+import img1 from './images/1.jpg';
+import img2 from './images/2.jpg';
+import img3 from './images/3.jpg';
+import img4 from './images/4.jpg';
+import img5 from './images/5.jpg';
+import img6 from './images/6.jpg';
 
 function App() {
+	const [currentImage, setGlobalCurrentImage] = useState(img1);
+	const [isOpenTab, setIsOpenTab] = useState(false);
 
-    const [currentImage, setGlobalCurrentImage] = useState();
-    const [isOpenTab, setIsOpenTab] = useState(false);
+	const images = [img1, img2, img3, img4, img5, img6];
 
-    const images = [img1, img2, img3, img4];
-
-    return (
-        <div className="gallery">
-            <div className="gallery__mainWindow">
-                <img className="gallery__mainPicture" src={currentImage} alt="test"/>
-                {isOpenTab && <GalleryTab currentImage={currentImage} setGlobalCurrentImage={setGlobalCurrentImage} images={images}/>}
-            </div>
-            <NavigationBar currImg={currentImage} setGlobalCurrentImage={setGlobalCurrentImage} images={images}/>
-            <ExpandGalleryBtn isOpen={isOpenTab} switchAction={setIsOpenTab} />
-        </div>
-    );
+	return (
+		<div className="gallery">
+			<div className="gallery__mainWindow">
+				<div
+					className="gallery__mainPicture"
+					style={{ backgroundImage: `url(${currentImage})` }}
+				></div>
+				{isOpenTab && (
+					<GalleryTab
+						currentImage={currentImage}
+						setGlobalCurrentImage={setGlobalCurrentImage}
+						images={images}
+					/>
+				)}
+			</div>
+			<NavigationBar
+				currImg={currentImage}
+				setGlobalCurrentImage={setGlobalCurrentImage}
+				images={images}
+			/>
+			<ExpandGalleryBtn isOpen={isOpenTab} switchAction={setIsOpenTab} />
+		</div>
+	);
 }
 
 export default App;
--- src/organisms/NavigationBar.js
+++ src/organisms/NavigationBar.js
@@ -1,59 +1,96 @@
-import {useState, useEffect} from 'react';
-import Chevron from "../molecules/Chevron";
+import { useState, useEffect } from 'react';
+import Chevron from '../molecules/Chevron';
 
-function NavigationBar({currImg, images, setGlobalCurrentImage}) {
-    const [showedPictures, setShowedPictures] = useState(images.slice(0, 3));
-    const [currentIndexImage, setCurrentIndexImage] = useState(currImg ? images.findIndex(el => el === currImg) : 0);
+function NavigationBar({ currImg, images, setGlobalCurrentImage }) {
+	const findNearestPositions = (images, newCurrImg, isEdge) => {
+		const index = images.findIndex((img) => img === newCurrImg);
 
-    const switchPictureRight = (isRight) => {
-        let baseImages = [...showedPictures];
+		if (isEdge) {
+			if (index === images.length - 1) {
+				setCurrentIndexImage(1);
+				return setShowedPictures([
+					images[images.length - 2],
+					images[index],
+					images[0],
+				]);
+			} else if (index === 0) {
+				setCurrentIndexImage(1);
+				return setShowedPictures(
+					images[images.length - 1],
+					images[index],
+					images[1]
+				);
+			}
+		}
 
-        if (isRight) {
-            if (currentIndexImage === 2) {
-                baseImages = baseImages.splice(1).concat(baseImages);
-            } else {
-                setCurrentIndexImage(currentIndexImage + 1)
-            }
-        } else {
-            if (currentIndexImage === 0) {
-                baseImages = baseImages.splice(-1, 1).concat(baseImages);
-            } else {
-                setCurrentIndexImage(currentIndexImage - 1);
-            }
-        }
-        setShowedPictures(baseImages);
-    }
+		setCurrentIndexImage(1);
+		return [images[index - 1], images[index], images[index + 1]];
+	};
 
+	const [showedPictures, setShowedPictures] = useState(images.slice(0, 3));
+	const [currentIndexImage, setCurrentIndexImage] = useState(
+		currImg ? images.findIndex((el) => el === currImg) : 0
+	);
 
-    useEffect(() => {
-        setGlobalCurrentImage(showedPictures[currentIndexImage])
-    }, [currentIndexImage, showedPictures]);
+	const switchPictureRight = (isRight) => {
+		let baseImages = [...showedPictures];
+		if (isRight) {
+			if (currentIndexImage === 2) {
+				// baseImages = baseImages.splice(1).concat(baseImages);
+				// проверка на пространство
+				const index = images.findIndex((img) => img === currImg);
 
-    useEffect(() => {
-        const indexOfImg = showedPictures.findIndex((img)=>currImg === img);
+				if (index === images.length - 1) {
+					setGlobalCurrentImage(images[0]);
+					// baseImages = [images[images.length - 1], images[0], images[1]];
+				} else {
+					// baseImages = [images[index - 1], images[index], images[index + 1]];
+					setGlobalCurrentImage(images[index + 1]);
+				}
+			} else {
+				setCurrentIndexImage(currentIndexImage + 1);
+				setGlobalCurrentImage(baseImages[currentIndexImage + 1]);
+			}
+		} else {
+			if (currentIndexImage === 0) {
+				// baseImages = baseImages.splice(-1, 1).concat(baseImages);
+			} else {
+				setCurrentIndexImage(currentIndexImage - 1);
+				setGlobalCurrentImage(baseImages[currentIndexImage - 1]);
+			}
+		}
+		setShowedPictures(baseImages);
+	};
 
-        if (indexOfImg !== -1) {
-            setCurrentIndexImage(indexOfImg);
-        }
-        else {
-            const indexInGlobalArray = images.findIndex((img)=>currImg === img);
-            // setShowedPictures
-            const updatedImagesArray = images.splice(1).concat(baseImages);
-        }
-    }, [currImg]);
+	useEffect(() => {
+		const indexOfImg = showedPictures.findIndex((img) => currImg === img);
 
+		if (indexOfImg !== -1) {
+			setCurrentIndexImage(indexOfImg);
+		} else {
+			findNearestPositions(images, currImg, true);
+		}
+	}, [currImg]);
 
-    return (
-        <ul className="gallery__navigationBar">
-            {images.length > 3 && <Chevron onClick={() => switchPictureRight(false)} direction="left"/>}
-            {showedPictures.map((img, index) =>
-                <li key={index + '-' + img} onClick={() => setCurrentIndexImage(index)}
-                    className={currentIndexImage === index ? '-activeImgInLi' : ''}>
-                    <img src={img} alt=""/>
-                </li>)}
-            {images.length > 3 && <Chevron onClick={() => switchPictureRight(true)}/>}
-        </ul>
-    );
+	return (
+		<ul className="gallery__navigationBar">
+			{images.length > 3 && (
+				<Chevron onClick={() => switchPictureRight(false)} direction="left" />
+			)}
+			{showedPictures.map((img, index) => (
+				<li
+					key={index + '-' + img}
+					onClick={() => setGlobalCurrentImage(img)}
+					className={currentIndexImage === index ? '-activeImgInLi' : ''}
+				>
+					<img src={img} alt="" />
+				</li>
+			))}
+			{images.length > 3 && (
+				<Chevron onClick={() => switchPictureRight(true)} />
+			)}
+		</ul>
+	);
 }
 
 export default NavigationBar;
